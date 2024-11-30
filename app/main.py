09d5fa0ae3962c7836d71bfe90b48cc184f32b13
Andrew Kincaid <42046274+atkincaid74@users.noreply.github.com>
2024-11-29 19:19:52 -0600

    pipelines

--- app/main.py
+++ app/main.py
@@ -6,3 +6,9 @@ app = FastAPI()
 @app.get("/test")
 async def test_route():
     return {"message": "Fiona is about to be an All Star"}
+
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run(app, port=8069)
